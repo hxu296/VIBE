4febfe8ce717bbc8ea36f29d614310e5ac92eeef
hxu296 <61036578+hxu296@users.noreply.github.com>
2020-12-30 16:41:05 -0600

    Update load_3dpw.sh

--- scripts/load_3dpw.sh
+++ scripts/load_3dpw.sh
@@ -5,13 +5,13 @@ cd data/3dpw
 
 #sequenceFiles.zip
 gdown "https://drive.google.com/uc?id=1q6HxN0ZY8bm50sK4EBVImZSevGiAbNDS"
-unzip sequenceFiles.zip
+unzip -q sequenceFiles.zip
 rm sequenceFiles.zip
 rm -rf __MACOSX
 
 #imageFiles.zip
 gdown "https://drive.google.com/uc?id=1K_X-6Oju_LCjwLqcUU8Yxu0zPI3eyXRV"
-unzip imageFiles.zip
+unzip -q imageFiles.zip
 rm imageFiles.zip
 
 
